0d22f7f1b9173920f14b826daa1f1ef939b4158c
dabria <73hl10n@gmail.com>
2013-03-23 18:38:04 -0700

    Finish user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,10 +25,15 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-   private
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
-    def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
-    end
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 
 end
